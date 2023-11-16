11f7e94ebb9134e5e3ff772df29891528568f9ce
Ubuntu <elkhemlichi.mehdi@gmail.com>
2023-11-16 21:55:17 +0000

    ip fix

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
       $('div.amenities > h4').html('&nbsp;');
     }
   });
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+  $.get('http://35.174.176.151:5001/api/v1/status/', function (data, textStatus) {
     if (textStatus === 'success') {
       if (data.status === 'OK') {
         $('#api_status').addClass('available');
